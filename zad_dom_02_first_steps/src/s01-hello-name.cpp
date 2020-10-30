03332b0c66007108f2ac5744be8dd6510ef319d9
Zuzanna Bernacka <s22020@pjwstk.edu.pl>
2020-10-30 07:42:41 +0100

    Format to cleaner code and add necessary flags

--- zad_dom_02_first_steps/src/s01-hello-name.cpp
+++ zad_dom_02_first_steps/src/s01-hello-name.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 
-auto main () -> int {
+auto main() -> int
+{
     auto name = std::string{};
     std::getline(std::cin, name);
     std::cout << "Hello, " << name << "!\n";
