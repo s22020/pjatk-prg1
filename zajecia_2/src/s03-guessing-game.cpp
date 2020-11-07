f697b38c39294a7a0ec2648b0373dfc8a9c2f0f5
Zuzanna Bernacka <s22020@pjwstk.edu.pl>
2020-11-07 11:01:56 +0100

    Add guessing game

---
+++ zajecia_2/src/s03-guessing-game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <random>
+
+auto guess_number() -> int {
+    std::cout << "guess: ";
+    auto input = std::string{};
+    std::getline(std::cin, input);
+    return std::stoi(input);
+}
+
+auto main() -> int {
+    std::random_device rd;
+    std::uniform_int_distribution<int> rnd (1, 100);
+    auto const pc_number = rnd(rd);
+    while (true) {
+        auto user_number = guess_number();
+        if (user_number < pc_number) {
+            std::cout << "number too small!\n";
+        } else if (user_number > pc_number) {
+            std::cout << "number too big!\n";
+        } else {
+            std::cout << "just right!\n";
+            break;
+        }
+    return 0;
+    }
+}
